7d2f8f32f0e9d74c2e10905a5bfca4ad9d54fc59
sikuda <sikuda@yandex.ru>
2021-11-07 18:38:31 +0300

    ViewBinding=true update to binding

--- app/src/main/java/ru/sikuda/mobile/start44_listview/MainActivity.kt
+++ app/src/main/java/ru/sikuda/mobile/start44_listview/MainActivity.kt
@@ -2,88 +2,60 @@ package ru.sikuda.mobile.start44_listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.R.layout
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ListView
-import android.widget.AdapterView
+import android.widget.*
 import android.widget.AdapterView.OnItemClickListener
-import android.widget.AbsListView
+import ru.sikuda.mobile.start44_listview.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+class MainActivity : AppCompatActivity(){
     val LOG_TAG = "myLogs"
-    var lvMain: ListView? = null
-    var names = arrayOf(
-        "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
-        "Костя", "Игорь", "Анна", "Денис", "Андрей"
-    )
+
+    lateinit var binding: ActivityMainBinding
+    //lateinit var lvMain: ListView
+//    var names = arrayOf(
+//        "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+//        "Костя", "Игорь", "Анна", "Денис", "Андрей"
+//    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-//      //lesson 42
-//      val lvMain: ListView = findViewById<View>(R.id.lvMain) as ListView
-//
-//      val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
-//            this,
-//            R.layout.simple_list_item_1, names
-//      )
-//      lvMain.setAdapter(adapter)
+        //lesson 42 - new binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val viewAll = binding.root
+        setContentView(viewAll)
 
         //lesson 43
-//        val adapter = ArrayAdapter.createFromResource(
-//            this, R.array.names,
-//            layout.simple_list_item_single_choice
-//        )
-//        lvMain = findViewById<View>(R.id.lvMain) as ListView?
-//        lvMain?.run {
-//            //choiceMode = ListView.CHOICE_MODE_SINGLE
-//            choiceMode = ListView.CHOICE_MODE_MULTIPLE
-//            setAdapter(adapter)
-//        }
-//        val btnChecked: Button = findViewById<View>(R.id.btnChecked) as Button
-//        btnChecked.setOnClickListener(this)
-//        names = resources.getStringArray(R.array.names)
+        val adapter = ArrayAdapter.createFromResource(this, R.array.names, layout.simple_list_item_1)
+        binding.lvMain.choiceMode = ListView.CHOICE_MODE_SINGLE //ListView.CHOICE_MODE_MULTIPLE
+        binding.lvMain.adapter = adapter
 
-        //lesson 44
-        lvMain = findViewById<View>(R.id.lvMain) as ListView
-
-        val adapter = ArrayAdapter.createFromResource(
-            this, R.array.names, layout.simple_list_item_1
-        )
-        lvMain!!.adapter = adapter
-
-        lvMain!!.onItemClickListener =
+        //OnItemSelectedListener is used for Spinners not for ListView
+        binding.lvMain.onItemClickListener =
             OnItemClickListener { parent, view, position, id ->
-                Log.d(
-                    LOG_TAG, "itemClick: position = " + position + ", id = "
-                            + id
-                )
+                showToast("itemClick: position = " + position + ", id = "+id)
             }
 
-        lvMain!!.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        //lesson 44
+        binding.lvMain.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?, view: View?,
                 position: Int, id: Long
             ) {
-                Log.d(
-                    LOG_TAG, "itemSelect: position = " + position + ", id = "
-                            + id
-                )
+                showToast("itemSelect: position = " + position + ", id = "+id)
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                Log.d(LOG_TAG, "itemSelect: nothing")
+                showToast("itemSelect: nothing")
+                //Log.d(LOG_TAG, "itemSelect: nothing")
             }
 
         }
 
-        lvMain!!.setOnScrollListener(object : AbsListView.OnScrollListener {
+        binding.lvMain.setOnScrollListener(object : AbsListView.OnScrollListener {
             override fun onScrollStateChanged(view: AbsListView?, scrollState: Int) {
                 Log.d(LOG_TAG, "scrollState = $scrollState")
             }
@@ -99,19 +71,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 )
             }
         })
+
+        binding.btnChecked.setOnClickListener{
+            val sbArray = binding.lvMain.checkedItemPositions
+            var str = ""
+            for (i in 0 until sbArray.size()) {
+                val key = sbArray.keyAt(i)
+                if (sbArray[key]) {
+                    str += key.toString()+","
+                    Log.d(LOG_TAG, key.toString())
+                }
+             }
+            showToast("Selected:"+str)
+        }
     }
 
-    override fun onClick(v: View?) {
-        //Log.d(LOG_TAG, "checked: " + names[lvMain?.getCheckedItemPosition()!!]);
-        //Log.d(LOG_TAG, "checked: " + lvMain?.getCheckedItemPosition())
-        Log.d(LOG_TAG, "checked: ")
-        val sbArray = lvMain!!.checkedItemPositions
-        for (i in 0 until sbArray.size()) {
-            val key = sbArray.keyAt(i)
-            if (sbArray[key]) {
-                Log.d(LOG_TAG, names[key])
-            }
-        }
+    fun showToast(str: String){
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
 }
 
